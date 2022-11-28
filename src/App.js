ec8d4f08bd067868a2fb03f8ca40f5b4acbd6669
gabby-trilling <113384370+gabby-trilling@users.noreply.github.com>
2022-11-28 18:42:28 -0500

    half of tutorial

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
+import NasaPhoto from "./components/NasaPhoto";
 import LineChart from "./LineChart";
 import Chart from './ResponsiveBar';
 
 function App() {
   return (
-    <div className="App">
+    
+    <BrowserRouter>
+      <div>
+        <Route component={Home} path="/" exact/>
+        <Route component={NasaPhoto} path="/nasaphoto" />
+      </div>
+    </BrowserRouter>
+    
+    
+    /* <div className="App">
       <header className="App-header">
         <LineChart />
         <Chart />
       </header>
-    </div>
+    </div> /*
   );
 }
 
