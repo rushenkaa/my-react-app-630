a9a75fefb806917d7af98c65af21de78a085c6e6
rushenkaa <93442725+rushenkaa@users.noreply.github.com>
2022-11-28 20:31:58 -0500

    done nasa

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import './App.css';
-import LineChart from "./LineChart";
-import Chart from './ResponsiveBar';
+document.querySelector('button').addEventListener('click', getFetch)
+function getFetch(){
+    const choice = document.querySelector('input').value
+    console.log(choice)
+    const url = `https://api.nasa.gov/planetary/apod?api_key=aG5AdhEqeSmwERedGPPweN2MmYZo7AwEB2EzgeSB&date=${choice}`
+    console.log(url)
+    fetch(url)
+    .then(res => res.json()) //to parse data as json
+    .then(data =>{
+        console.log(data)
+        document.querySelector('img').src = data.hdurl
+        document.querySelector('h3').innerText = data.explanation
+    })
+    .catch(err =>{
+        console.log('error ${err}')
+    });
+}
 
 function App() {
   return (
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ document.querySelector('button').addEventListener('click', getFetch)
 function getFetch(){
     const choice = document.querySelector('input').value
     console.log(choice)
-    const url = 'https://api.nasa.gov/planetary/apod?api_key=aG5AdhEqeSmwERedGPPweN2MmYZo7AwEB2EzgeSB&date=${choice}'
+    const url = `https://api.nasa.gov/planetary/apod?api_key=aG5AdhEqeSmwERedGPPweN2MmYZo7AwEB2EzgeSB&date=${choice}`
     fetch(url)
     .then(res => res.json()) //to parse data as json
     .then(data =>{
@@ -11,6 +11,8 @@ function getFetch(){
         document.querySelector('h3').innerText = data.explanation
     })
     .catch(err =>{
+        // eslint-disable-next-line
         console.log('error ${err}')
     });
+    // eslint-disable-next-line
 }
